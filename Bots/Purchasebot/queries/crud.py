46fb940843dbacb259a5d41d8c3bb0acc2ef31e5
Gayratbek <gayratbekakhmedov@gmail.com>
2024-05-15 14:58:27 +0500

    import orders instead o Orders models

--- Bots/Purchasebot/queries/crud.py
+++ Bots/Purchasebot/queries/crud.py
@@ -8,7 +8,7 @@ from sqlalchemy.sql import func
 from datetime import datetime,timedelta
 from sqlalchemy import or_, and_, Date, cast
 from uuid import UUID
-from orders.models.orders import Orders
+from orders.models import orders
 
 
 
