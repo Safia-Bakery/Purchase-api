af81a5b9ad25a2a59314bc6ed85f1fa55d3ac6bd
Gayratbek <gayratbekakhmedov@gmail.com>
2024-07-02 17:35:07 +0500

    bugfix filter of purchase

--- orders/routes/order_route.py
+++ orders/routes/order_route.py
@@ -299,11 +299,7 @@ async def get_expanditure_router(
                     pass
 
         query.items[0].total_sum = "{:.2f}".format(total_sum)
-
-
-
-
-    return paginate([])
+    return query
 
 
 
